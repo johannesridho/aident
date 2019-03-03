d34eb7150af00d3f20dd9f61611d824dc1a52b2b
fakhri <fakhri.abdr@gmail.com>
2019-03-03 15:49:49 +0700

    Refactor and add more suspicious activity

--- lambda/src/main/java/com/amazonaws/lambda/demo/JobCompletionHandler.java
+++ lambda/src/main/java/com/amazonaws/lambda/demo/JobCompletionHandler.java
@@ -28,131 +28,129 @@ import com.amazonaws.services.sns.model.PublishResult;
 
 public class JobCompletionHandler implements RequestHandler<SNSEvent, String> {
 
-   @Override
-   public String handleRequest(SNSEvent event, Context context) {
+	@Override
+	public String handleRequest(SNSEvent event, Context context) {
 
-      String message = event.getRecords().get(0).getSNS().getMessage();
-      LambdaLogger logger = context.getLogger();
+		String message = event.getRecords().get(0).getSNS().getMessage();
+		LambdaLogger logger = context.getLogger();
 
-      // Parse SNS event for analysis results. Log results
-      try {
-         ObjectMapper operationResultMapper = new ObjectMapper();
-         JsonNode jsonResultTree = operationResultMapper.readTree(message);
-         logger.log("Rekognition Video Operation:=========================");
-         logger.log("Job id: " + jsonResultTree.get("JobId"));
-         logger.log("Status : " + jsonResultTree.get("Status"));
-         logger.log("Job tag : " + jsonResultTree.get("JobTag"));
-         logger.log("Operation : " + jsonResultTree.get("API"));
+		// Parse SNS event for analysis results. Log results
+		try {
+			ObjectMapper operationResultMapper = new ObjectMapper();
+			JsonNode jsonResultTree = operationResultMapper.readTree(message);
+			logger.log("Rekognition Video Operation:=========================");
+			logger.log("Job id: " + jsonResultTree.get("JobId"));
+			logger.log("Status : " + jsonResultTree.get("Status"));
+			logger.log("Job tag : " + jsonResultTree.get("JobTag"));
+			logger.log("Operation : " + jsonResultTree.get("API"));
 
-         if (jsonResultTree.get("API").asText().equals("StartLabelDetection")) {
+			if (jsonResultTree.get("API").asText().equals("StartLabelDetection")) {
 
-            if (jsonResultTree.get("Status").asText().equals("SUCCEEDED")){
-               GetResultsLabels(jsonResultTree.get("JobId").asText(), context);
-            }
-            else{
-               String errorMessage = "Video analysis failed for job " 
-                     + jsonResultTree.get("JobId") 
-                     + "State " + jsonResultTree.get("Status");
-               throw new Exception(errorMessage); 
-            }
+				if (jsonResultTree.get("Status").asText().equals("SUCCEEDED")){
+					GetResultsLabels(jsonResultTree.get("JobId").asText(), context);
+				}
+				else{
+					String errorMessage = "Video analysis failed for job " 
+							+ jsonResultTree.get("JobId") 
+							+ "State " + jsonResultTree.get("Status");
+					throw new Exception(errorMessage); 
+				}
 
-         } else
-            logger.log("Operation not StartLabelDetection");
+			} else {
+				logger.log("Operation not StartLabelDetection");
+			}
 
-      } catch (Exception e) {
-         logger.log("Error: " + e.getMessage());
-         throw new RuntimeException (e);
+		} catch (Exception e) {
+			logger.log("Error: " + e.getMessage());
+			throw new RuntimeException (e);
+		}
 
+		return message;
+	}
 
-      }
+	void GetResultsLabels(String startJobId, Context context) throws Exception {
 
-      return message;
-   }
+		LambdaLogger logger = context.getLogger();
 
-   void GetResultsLabels(String startJobId, Context context) throws Exception {
+		AmazonRekognition rek = AmazonRekognitionClientBuilder.standard().withRegion(Regions.AP_NORTHEAST_2).build();
 
-      LambdaLogger logger = context.getLogger();
+		int maxResults = 1000;
+		String paginationToken = null;
+		GetLabelDetectionResult labelDetectionResult = null;
+		String labels = "";
+		Integer labelsCount = 0;
+		String label = "";
+		String currentLabel = "";
+		Integer suspiciousLabelsCount = 0;
+		String suspiciousLabel = "";
+		List<String> allSuspiciousLabels = getSuspiciousLabels();
 
-      AmazonRekognition rek = AmazonRekognitionClientBuilder.standard().withRegion(Regions.AP_NORTHEAST_2).build();
+		//Get label detection results and log them. 
+		do {
 
-      int maxResults = 1000;
-      String paginationToken = null;
-      GetLabelDetectionResult labelDetectionResult = null;
-      String labels = "";
-      Integer labelsCount = 0;
-      String label = "";
-      String currentLabel = "";
-      Integer suspiciousLabelsCount = 0;
-      String suspiciousLabel = "";
-      List<String> allSuspiciousLabels = getSuspiciousLabels();
-     
-      //Get label detection results and log them. 
-      do {
+			GetLabelDetectionRequest labelDetectionRequest = new GetLabelDetectionRequest().withJobId(startJobId)
+					.withSortBy(LabelDetectionSortBy.NAME).withMaxResults(maxResults).withNextToken(paginationToken);
 
-         GetLabelDetectionRequest labelDetectionRequest = new GetLabelDetectionRequest().withJobId(startJobId)
-               .withSortBy(LabelDetectionSortBy.NAME).withMaxResults(maxResults).withNextToken(paginationToken);
+			labelDetectionResult = rek.getLabelDetection(labelDetectionRequest);
 
-         labelDetectionResult = rek.getLabelDetection(labelDetectionRequest);
-         
-         paginationToken = labelDetectionResult.getNextToken();
-         VideoMetadata videoMetaData = labelDetectionResult.getVideoMetadata();
+			paginationToken = labelDetectionResult.getNextToken();
+			VideoMetadata videoMetaData = labelDetectionResult.getVideoMetadata();
 
-         // Add labels to log
-         List<LabelDetection> detectedLabels = labelDetectionResult.getLabels();
-         
-         for (LabelDetection detectedLabel : detectedLabels) {
-            label = detectedLabel.getLabel().getName();
-            if (label.equals(currentLabel)) {
-               continue;
-            }
-            labels = labels + label + " / ";
-            currentLabel = label;
-            labelsCount++;
+			// Add labels to log
+			List<LabelDetection> detectedLabels = labelDetectionResult.getLabels();
 
-            if (allSuspiciousLabels.contains(label) ) {
-            	suspiciousLabel = suspiciousLabel + label + " / ";
-            	suspiciousLabelsCount++;
-            }
-         }
-      } while (labelDetectionResult != null && labelDetectionResult.getNextToken() != null);
-      
-      if (suspiciousLabelsCount > 0) {
-    	  publishSNS(label, context);
-	  }
-      
-      logger.log("Total number of labels : " + labelsCount);
-      logger.log("labels : " + labels);
+			for (LabelDetection detectedLabel : detectedLabels) {
+				label = detectedLabel.getLabel().getName();
+				if (label.equals(currentLabel)) {
+					continue;
+				}
+				labels = labels + label + " / ";
+				currentLabel = label;
+				labelsCount++;
 
-      logger.log("Total number of suspicious labels : " + suspiciousLabelsCount);
-      logger.log("suspicious labels : " + suspiciousLabel);
-   }
+				if (allSuspiciousLabels.contains(label.toLowerCase().replaceAll("\\s+","-")) ) {
+					suspiciousLabel = suspiciousLabel + label + " / ";
+					suspiciousLabelsCount++;
+				}
+			}
+		} while (labelDetectionResult != null && labelDetectionResult.getNextToken() != null);
 
+		if (suspiciousLabelsCount > 0) {
+			publishSNS(label, context);
+		}
 
-   List<String> getSuspiciousLabels() {
-	      List<String> suspiciousLabels = new ArrayList<>();
-	      suspiciousLabels.add("kicking");
-	      suspiciousLabels.add("punching");
-	      suspiciousLabels.add("fighting");
-	      suspiciousLabels.add("martial art");
-	      suspiciousLabels.add("wrestling");
-	      
-	      return suspiciousLabels;
-   }
-   
-   void publishSNS(String label, Context context) {
-	   LambdaLogger logger = context.getLogger();
-	   AmazonSNS snsClient = AmazonSNSClientBuilder.standard().withRegion(Regions.AP_NORTHEAST_2).build();
-	   
-	   String topicArn = "arn:aws:sns:ap-northeast-2:964962553544:Email";
-	   
-	   //publish to an SNS topic
-	   String msg = "Suspicious activity detected with label: " + label;
-	   PublishRequest publishRequest = new PublishRequest(topicArn, msg);
-	   PublishResult publishResult = snsClient.publish(publishRequest);
-	   //print MessageId of message published to SNS topic
-	   logger.log("MessageId - " + publishResult.getMessageId());
+		logger.log("Total number of labels : " + labelsCount);
+		logger.log("labels : " + labels);
 
-   }
+		logger.log("Total number of suspicious labels : " + suspiciousLabelsCount);
+		logger.log("suspicious labels : " + suspiciousLabel);
+	}
+
+	void publishSNS(String label, Context context) {
+		LambdaLogger logger = context.getLogger();
+		AmazonSNS snsClient = AmazonSNSClientBuilder.standard().withRegion(Regions.AP_NORTHEAST_2).build();
+
+		String topicArn = "arn:aws:sns:ap-northeast-2:964962553544:Email";
+
+		//publish to an SNS topic
+		String msg = "Suspicious activity detected with label: " + label;
+		PublishRequest publishRequest = new PublishRequest(topicArn, msg);
+		PublishResult publishResult = snsClient.publish(publishRequest);
+		//print MessageId of message published to SNS topic
+		logger.log("MessageId - " + publishResult.getMessageId());
+	}
+
+	List<String> getSuspiciousLabels() {
+		List<String> suspiciousLabels = new ArrayList<>();
+		suspiciousLabels.add("kicking");
+		suspiciousLabels.add("punching");
+		suspiciousLabels.add("fighting");
+		suspiciousLabels.add("martial-art");
+		suspiciousLabels.add("wrestling");
+		suspiciousLabels.add("boxing");
+
+		return suspiciousLabels;
+	}
 }
 
 
